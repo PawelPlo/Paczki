b4e76fb637931a37f0d7408142b21518f9c457ac
Pawel Plociennik <p.plociennik@wp.pl>
2023-04-25 21:43:37 +0200

    Zatrzymanie przy 20 kg

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ liczba_przesylek=input("Ile przesylek chcesz wyslac?    ")
 liczba_przesylek=int(liczba_przesylek)
 paczka = 0
 paczka = int(paczka)
+maxpojemnosc_paczki = 20
+maxpojemnosc_paczki = int(maxpojemnosc_paczki)
 
 for waga_przesylki in range(liczba_przesylek):
     waga_przesylki = int(input("Podaj wage przesylki min 1kg - max 10kg:   "))
@@ -12,12 +14,11 @@ for waga_przesylki in range(liczba_przesylek):
         break
 
     elif waga_przesylki >1 or waga_przesylki <10:
-         paczka += waga_przesylki
-         print(paczka)
-
-    if paczka > 20:
-        print("cos")
-        break
+        paczka += waga_przesylki
+        if not paczka > 20:
+            print(paczka)
+        else:
+            print("Do pierwszej paczki zapakowano {} kg, nie wykorzystano {} kg".format(paczka, maxpojemnosc_paczki-paczka))
 
 
 
