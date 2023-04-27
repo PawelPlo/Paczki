0236e9fbc559c7c350ef436c6a3b0200a4b2e91b
Pawel Plociennik <p.plociennik@wp.pl>
2023-04-27 17:34:16 +0200

    Dodano odejmowanie elementu po przekroczeniu 20 kg

--- main.py
+++ main.py
@@ -4,21 +4,35 @@ liczba_przesylek=input("Ile przesylek chcesz wyslac?    ")
 liczba_przesylek=int(liczba_przesylek)
 paczka = 0
 paczka = int(paczka)
-maxpojemnosc_paczki = 20
-maxpojemnosc_paczki = int(maxpojemnosc_paczki)
+
+
 
 for waga_przesylki in range(liczba_przesylek):
     waga_przesylki = int(input("Podaj wage przesylki min 1kg - max 10kg:   "))
+
     if waga_przesylki <1 or waga_przesylki >10:
         print("Twoje przesylki wyslano, poza ostatnia")
         break
 
     elif waga_przesylki >1 or waga_przesylki <10:
         paczka += waga_przesylki
-        if not paczka > 20:
-            print(paczka)
-        else:
-            print("Do pierwszej paczki zapakowano {} kg, nie wykorzystano {} kg".format(paczka, maxpojemnosc_paczki-paczka))
+        print(paczka)
+
+        if paczka >= 20:
+            print("Waga twojej paczki przekroczyla 20 kg. Wyjmij ostatni element i dodaj do następnej paczki")
+            ostatni_element=int(input("Podaj wage ostatniego elementu w kg:   "))
+            paczka -= ostatni_element
+            print("wyslano paczke o wadze {} kg".format(paczka))
+            waga_przesylki = int(input("Podaj wage przesylki min 1kg - max 10kg:   "))
+
+print(paczka)
+
+#   print("Do pierwszej paczki zapakowano {} kg, nie wykorzystano {} kg".format(paczka))
+
+
+
+         #else:
+         #   print("Do pierwszej paczki zapakowano {} kg, nie wykorzystano {} kg".format(paczka, maxpojemnosc_paczki-paczka))
 
 
 
