a41c7c2d94e62527da1180c170f5635c1b0b14ed
Pawel Plociennik <p.plociennik@wp.pl>
2023-04-29 13:12:08 +0200

    Dodano numeracje przesylki, informacje o aktualnej wadze paczki, wadze przeniesionej do nastepnej przesylki, program liczy poprawnie

--- main.py
+++ main.py
@@ -2,30 +2,45 @@
 
 liczba_przesylek=input("Ile przesylek chcesz wyslac?    ")
 liczba_przesylek=int(liczba_przesylek)
+nr_przesylki=1
+nr_przesylki=int(nr_przesylki)
+pudelko = 0
+pudelko = int(pudelko)
 paczka = 0
 paczka = int(paczka)
-
+poj_pudelka = 20
+poj_pudelka = int(poj_pudelka)
 
 
 for waga_przesylki in range(liczba_przesylek):
-    waga_przesylki = int(input("Podaj wage przesylki min 1kg - max 10kg:   "))
-
+    waga_przesylki = int(input("Podaj wage przesylki nr {} min 1kg - max 10kg:   ".format(nr_przesylki)))
     if waga_przesylki <1 or waga_przesylki >10:
-        print("Twoje przesylki wyslano, poza ostatnia")
+        print("Zla waga przesylki, koniec przyjmowania")
         break
-
-    elif waga_przesylki >1 or waga_przesylki <10:
+    if waga_przesylki >1 or waga_przesylki <10:
         paczka += waga_przesylki
-        print(paczka)
+        nr_przesylki = nr_przesylki + 1
+        if paczka > 20:
+            paczka -= waga_przesylki
+            pudelko = pudelko + 1
+            print("wyslano 1 pudelko o wadze {}".format(paczka))
+            print("W wyslanej paczce pozostalo {} wolnych kilogramow".format(poj_pudelka-paczka))
+            paczka = 0 + waga_przesylki
+            print("Do nastepnej paczki przeniesiono {} kg".format(paczka))
+            continue
+        if paczka == 20:
+            pudelko = pudelko + 1
+            print("wyslano 1 pudelko o wadze 20 kg")
+            paczka=0
+            print("W paczce pozostalo {} wolnych kilogramow".format(poj_pudelka - paczka))
+            continue
+    else:
+        continue
+pudelko = pudelko + 1
+print("wyslano 1 pudelko o wadze {}".format(paczka))
+print("W wyslanej paczce pozostalo {} wolnych kilogramow".format(poj_pudelka-paczka))
+print("Wyslano {} sztuk pudelek o wadze {} kg".format(pudelko, paczka))
 
-        if paczka >= 20:
-            print("Waga twojej paczki przekroczyla 20 kg. Wyjmij ostatni element i dodaj do następnej paczki")
-            ostatni_element=int(input("Podaj wage ostatniego elementu w kg:   "))
-            paczka -= ostatni_element
-            print("wyslano paczke o wadze {} kg".format(paczka))
-            waga_przesylki = int(input("Podaj wage przesylki min 1kg - max 10kg:   "))
-
-print(paczka)
 
 #   print("Do pierwszej paczki zapakowano {} kg, nie wykorzystano {} kg".format(paczka))
 
